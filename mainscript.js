0d278f7ce357c07f62a25f526cbdb293bcbc94ca
tjschool <45406017+tjschool@users.noreply.github.com>
2018-12-31 16:04:28 -0500

    Add files via upload

---
+++ mainscript.js
@@ -0,0 +1,16 @@
+
+
+function resizeIFrameToFitContent( iFrame ) {
+
+    iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
+    iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
+}
+
+window.addEventListener('DOMContentLoaded', function(e) {
+
+    var iframes = document.querySelectorAll("iframe");
+    for( var i = 0; i < iframes.length; i++) {
+        resizeIFrameToFitContent( iframes[i] );
+    }
+} );
+
